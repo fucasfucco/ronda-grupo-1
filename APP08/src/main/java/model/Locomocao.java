323257cc326118b00c7f43ac405d2ac314e69f38
gusta <gusta@DESKTOP-AA7K9AU>
2021-06-02 15:04:34 -0300

    Model locomocao

---
+++ APP08/src/main/java/model/Locomocao.java
@@ -0,0 +1,105 @@
+package model;
+
+import java.io.Serializable;
+
+@Entity
+
+public class Locomocao implements Serializable {  
+	@Id
+	private Integer id;
+	private String descricao;
+	private String placa;
+	
+	private static final long serialVersionUID = 1L;
+
+	public Locomocao() {
+		super();
+	}
+
+	public Locomocao(Integer id, String descricao, String placa) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.placa = placa;
+	}
+
+	public Locomocao () {
+		super();
+	}
+
+	public Locomocao(Integer id) {
+		super();
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((placa == null) ? 0 : placa.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Locomocao other = (Locomocao) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (placa == null) {
+			if (other.placa != null)
+				return false;
+		} else if (!placa.equals(other.placa))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Locomocao [id=" + id + ", descricao=" + descricao + ", placa=" + placa + "]";
+	}
+	
+	
+	
+	
+	
+}
